b417c3587f017b0b573c49be46f1d0ce06a042b6
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-08 15:04:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,11 +18,12 @@ class Transfer
   end
 
   def execute_transaction
-    #binding.pry
+
     if self.status == "pending" && sender.balance >= self.amount && sender.status != "closed"
       sender.balance = sender.balance - self.amount
       receiver.balance = receiver.balance + self.amount
       self.status = "complete"
+      binding.pry
     else
       "Transaction rejected. Please check your account balance."
       self.status = "rejected"
